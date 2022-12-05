8d8c9117d6a63dccd6e885ae4188d6a1cd9405be
Ikhsanul29 <ikhsanulabbror@gmail.com>
2022-12-05 14:51:21 +0700

    convert ci3

---
+++ application/views/vfooter.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://kit.fontawesome.com/9350614167.js" crossorigin="anonymous"></script>
+	<title>Tes Footer</title>
+</head>
+
+<body>
+	<footer>
+		<div class="row">
+			<div class="col">
+				<img src="assets/img/logo trash change.png" class="logo">
+			</div>
+		</div>
+		<div class="panduan">
+			<div class="kotakpanduan">
+				<div class="tulisankotakpanduan">
+					<a href="#">Akses panduan pengguna</a>
+				</div>
+			</div>
+		</div>
+		<div class="search">
+			<input type="text" class="form-control" placeholder="Lakukan Penelusuran Konten">
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
+			</span>
+		</div>
+	</footer>
+</body>
+
+</html>
